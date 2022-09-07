eb08cf83b78e43a0514e344e4774dfa6be120382
Suric1 <igorbronko@gmail.com>
2022-09-07 17:39:06 +0300

    добавление функции выбора предмета из списка

--- js/script.js
+++ js/script.js
@@ -1,12 +1,45 @@
 const dom = {
-  selectbox: document.getElementById('selectbox'),
-  rooms: document.getElementById('rooms'),
+	selectbox: document.getElementById('selectbox'),
+	selectboxList: document.querySelector('.selectbox__list'),
+	rooms: document.getElementById('rooms'),
+	
 };
 
-console.log(dom);
 
 //  Выпадающий список
 
 dom.selectbox.querySelector('.selectbox__selected').onclick = (event) => {
-  dom.selectbox.classList.toggle('open');
+	dom.selectbox.classList.toggle('open');
 };
+
+document.body.onclick = (event) => {
+	const {target} = event
+	if (
+		!target.matches('.selectbox')
+		&& !target.parentElement.matches('.selectbox')
+		&& !target.parentElement.parentElement.matches('.selectbox')
+	) {
+		dom.selectbox.classList.remove('open');
+	}
+}
+
+dom.selectboxList.onclick = (event) => {
+	const {target} = event
+	if (target.matches('.selectbox__item')) {
+		const {room} = target.dataset
+		const selectedItem = dom.selectboxList.querySelector('.selected')
+		selectedItem.classList.remove('selected')
+		target.classList.add('selected')
+		dom.selectbox.classList.remove('open')
+		selectRoom(room)
+	}
+}
+
+// Выбор комнаты
+
+function selectRoom(room) {
+	const selectedRoom = dom.rooms.querySelector('.selected')
+	if (selectedRoom) {
+		selectedRoom.classList.remove('selected');
+	}
+}
