46c0335beeeb88c17b0405a4a04e31585ca4103b
Suric1 <igorbronko@gmail.com>
2022-09-16 01:02:02 +0300

    добавлен 3-й экран интерфейс

--- js/script.js
+++ js/script.js
@@ -1,90 +1,102 @@
 const dom = {
-  selectbox: document.getElementById('selectbox'),
-  selectboxList: document.querySelector('.selectbox__list'),
-  rooms: document.getElementById('rooms'),
+	selectbox: document.getElementById('selectbox'),
+	selectboxList: document.querySelector('.selectbox__list'),
+	rooms: document.getElementById('rooms'),
+	settings: document.getElementById('settings'),
+	settingsTabs: document.getElementById('settings-tabs'),
+	settingsPanel: document.getElementById('settings-panel')
 };
 
 //  Выпадающий список
 
 dom.selectbox.querySelector('.selectbox__selected').onclick = (event) => {
-  dom.selectbox.classList.toggle('open');
+	dom.selectbox.classList.toggle('open');
 };
 
 document.body.onclick = (event) => {
-  const { target } = event;
-  if (
-    !target.matches('.selectbox') &&
-    !target.parentElement.matches('.selectbox') &&
-    !target.parentElement.parentElement.matches('.selectbox')
-  ) {
-    dom.selectbox.classList.remove('open');
-  }
+	const {target} = event;
+	if (
+		!target.matches('.selectbox') &&
+		!target.parentElement.matches('.selectbox') &&
+		!target.parentElement.parentElement.matches('.selectbox')
+	) {
+		dom.selectbox.classList.remove('open');
+	}
 };
 
 dom.selectboxList.onclick = (event) => {
-  const { target } = event;
-  if (target.matches('.selectbox__item')) {
-    const { room } = target.dataset;
-    const selectedItem = dom.selectboxList.querySelector('.selected');
-    selectedItem.classList.remove('selected');
-    target.classList.add('selected');
-    dom.selectbox.classList.remove('open');
-    selectRoom(room);
-  }
+	const {target} = event;
+	if (target.matches('.selectbox__item')) {
+		const {room} = target.dataset;
+		const selectedItem = dom.selectboxList.querySelector('.selected');
+		selectedItem.classList.remove('selected');
+		target.classList.add('selected');
+		dom.selectbox.classList.remove('open');
+		selectRoom(room);
+	}
 };
 const rooms = {
-  all: 'Все комнаты',
-  livingroom: 'Зал',
-  bedroom: 'Спальня',
-  kitchen: 'Кухня',
-  bathroom: 'Ванная',
-  studio: 'Кабинет',
-  washingroom: 'Уборная',
+	all: 'Все комнаты',
+	livingroom: 'Зал',
+	bedroom: 'Спальня',
+	kitchen: 'Кухня',
+	bathroom: 'Ванная',
+	studio: 'Кабинет',
+	washingroom: 'Уборная',
 };
 
 // Выбор комнаты
 
 function selectRoom(room) {
-  const selectedRoom = dom.rooms.querySelector('.selected');
-  if (selectedRoom) {
-    selectedRoom.classList.remove('selected');
-  }
-  if (room !== 'all') {
-    const newSelectedRoom = dom.rooms.querySelector(`[data-room=${room}]`);
-    newSelectedRoom.classList.add('selected');
-    renderScreen(false);
-  } else {
-    renderScreen(true);
-  }
-  const selectedSelectboxRoom = dom.selectbox.querySelector(
-    '.selectbox__item.selected'
-  );
-  selectedSelectboxRoom.classList.remove('selected');
-  const newSelectedItem = dom.selectbox.querySelector(`[data-room=${room}]`);
-  newSelectedItem.classList.add('selected');
-  const selectboxSelected = dom.selectbox.querySelector(
-    '.selectbox__selected  span'
-  );
-  selectboxSelected.innerHTML = rooms[room];
+	const selectedRoom = dom.rooms.querySelector('.selected');
+	if (selectedRoom) {
+		selectedRoom.classList.remove('selected');
+	}
+	if (room !== 'all') {
+		const newSelectedRoom = dom.rooms.querySelector(`[data-room=${room}]`);
+		newSelectedRoom.classList.add('selected');
+		renderScreen(false);
+	} else {
+		renderScreen(true);
+	}
+	const selectedSelectboxRoom = dom.selectbox.querySelector(
+		'.selectbox__item.selected'
+	);
+	selectedSelectboxRoom.classList.remove('selected');
+	const newSelectedItem = dom.selectbox.querySelector(`[data-room=${room}]`);
+	newSelectedItem.classList.add('selected');
+	const selectboxSelected = dom.selectbox.querySelector(
+		'.selectbox__selected  span'
+	);
+	selectboxSelected.innerHTML = rooms[room];
 }
 
 // Клик по элементу комнаты
 
 dom.rooms.querySelectorAll('.room').forEach((room) => {
-  room.onclick = (event) => {
-    const value = room.dataset.room;
-    selectRoom(value);
-  };
+	room.onclick = (event) => {
+		const value = room.dataset.room;
+		selectRoom(value);
+	};
 });
 
 // Отображение нужного экрана
 
 function renderScreen(isRooms) {
-  setTimeout(() => {
-    if (isRooms) {
-      dom.rooms.style.display = 'grid';
-    } else {
-      dom.rooms.style.display = 'none';
-    }
-  }, 300);
+	setTimeout(() => {
+		if (isRooms) {
+			dom.rooms.style.display = 'grid';
+			dom.settings.style.display = 'none'
+		} else {
+			dom.rooms.style.display = 'none';
+			dom.settings.style.display = 'block'
+		}
+	}, 300);
 }
+
+// Панель Настроек Комнаты
+
+
+
+
+
